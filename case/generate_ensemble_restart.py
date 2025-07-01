f07948523c44704b8e7d47a8256a8fbb5641371c
Hakase Hayashida <hakaseh@pokepoke.local>
2025-07-01 10:21:35 +0900

    deleting deleted files by "git add -u".

--- case/generate_ensemble_restart.py
+++
@@ -1,8 +0,0 @@
-import eatpy
-import numpy as np
-N = 10    # ensemble size
-with eatpy.models.gotm.RestartEnsemble("restart.nc", N) as f:
-    for name, values in f.template.items():
-        if name not in ("h", "z", "zi", "lon", "lat"):
-            scale_factors = np.random.lognormal(sigma=0.1, size=(N,) + values.shape)
-            f[name] = values * scale_factors
--- case/run_sensitivity.py
+++
@@ -1,6 +0,0 @@
-import eatpy
-experiment = eatpy.models.GOTM()
-filter = eatpy.PDAF(eatpy.pdaf.FilterType.ESTKF)
-# If you comment out the line below, you run the ensemble only, without assimilation
-#experiment.add_observations("temp[-1]", "cci_sst.dat")
-experiment.run(filter)
